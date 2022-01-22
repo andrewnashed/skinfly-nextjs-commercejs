e83a35340d29462d6954966411efff3edf9fd554
Andrew <cyberandrew30@yahoo.com>
2022-01-21 20:14:54 -0500

    adding prop key to likes page

--- pages/likes.js
+++ pages/likes.js
@@ -24,7 +24,7 @@ export default function LikesPage(){
         <div className='my-24 w-full flex flex-col items-center space-y-10'>
         <h2 className='text-3xl font-bold'>Products You've liked</h2>
         <section className='flex flex-wrap justify-evenly sm:w-full lg:w-[50%]'>
-        {likedProducts.map(product => <Product product={product}/>)}
+        {likedProducts.map(product => <Product key={product.id} product={product}/>)}
         </section>
         </div>
         )
